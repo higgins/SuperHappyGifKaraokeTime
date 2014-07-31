b13a6830603c657316f1ebe23ba08ab5e4db50cf
Bruce Sullivan <bsullivan@beatsmusic.com>
2014-07-31 12:34:31 -0700

    Updating Lyrics

--- scripts/ui/stage.js
+++ scripts/ui/stage.js
@@ -9,8 +9,10 @@ define(function (require) {
 			gifSelector: '.gif',
 			titleSelector: '.title',
 			artistSelector: '.artist',
+			lyricsSelector: '.lyrics',
 			lyricSelector: '.lyrical-content',
-			closeSelector: '.close'
+			closeSelector: '.close',
+			singleLyricSelector: '[data-time]'
 		});
 
 		this.after('initialize', function () {
@@ -38,14 +40,43 @@ define(function (require) {
 		};
 
 		this.setStage = function (msg) {
-			var text = msg.lyrics;
-			var newText = msg.lyrics.replace(/\\n/gm, '<br>');
+			var self = this;
+			var text = this.parseText(msg.lyrics);
 			$('.wrapper').css({position: 'fixed'});
 			this.select('audioSelector')[0].src = 'music/'+ msg.asset;
 			this.select('gifSelector').css({backgroundImage: 'url(gifs/' + msg.gif +')'});
 			this.select('titleSelector').html(msg.title);
 			this.select('artistSelector').html(msg.artist);
-			this.select('lyricSelector').html(newText);
+			this.select('lyricSelector').html(text);
+			// set timer
+			window.setInterval(function () {
+				// get current time
+				var time = self.select('audioSelector')[0].currentTime;
+				// get all items
+				self.select('singleLyricSelector').each(function (key, item) {
+					// if current time is greater than id, highlight the item
+					if ($(this).data('time') <= time) {
+						$(this).addClass('highlight');
+					}
+				});
+				var elements = $('.highlight')
+				var scrollElement = $(elements[elements.length - 1]).position().top;
+				var top = self.select('lyricsSelector').scrollTop();
+
+				self.select('lyricsSelector').animate({scrollTop: top + scrollElement - 320});
+			}, 1000);
+		};
+
+		this.parseText = function (text) {
+			var html = '';
+
+			_.each(text, function (item) {
+				html += '<p data-time="' + item.starttime +'">';
+				html += item.content.replace(/\\n/gm, '<br>');
+				html += '</p>';
+			});
+
+			return html;
 		};
 
 		this.clearStage = function () {
@@ -55,6 +86,7 @@ define(function (require) {
 			this.select('titleSelector').html('');
 			this.select('artistSelector').html('');
 			this.select('lyricSelector').html('');
+			// Clear stage
 		};
 
 		this.closeStage = function () {
